e3211e602bf11713052e597f2d4a72b342ba6a4f
pawan sharma <pawan.ps43563@hotmail.com>
2021-10-13 11:57:53 +0530

    MInor changes made in controller

--- controllers/cscontroller.js
+++ controllers/cscontroller.js
@@ -1,36 +1,34 @@
 const sendResponse = require("../utils/sendResponse");
 
 const { Stack,
-    FOOTER_CONTENT_TYPE_UID,FOOTER_ENTRY_UID,
-    NAVBAR_CONTENT_TYPE_UID,NAVBAR_ENTRY_UID,
-    HOMEPAGE_CONTENT_TYPE_UID,HOMEPAGE_ENTRY_UID 
+    FOOTER_CONTENT_TYPE_UID,
+    NAVBAR_CONTENT_TYPE_UID,
+    HOMEPAGE_CONTENT_TYPE_UID
     } = require('../cms/stackconfig');
 
 
 
 const fetchResuableComponentdata=async(req,res)=>{
-    let CONTENT_TYPE_UID,ENTRY_UID;
+    let CONTENT_TYPE_UID;
     switch(req.url){
         case '/footer':
             CONTENT_TYPE_UID=FOOTER_CONTENT_TYPE_UID;
-            ENTRY_UID=FOOTER_ENTRY_UID;
             break;
         case '/navbar':
             CONTENT_TYPE_UID=NAVBAR_CONTENT_TYPE_UID;
-            ENTRY_UID=NAVBAR_ENTRY_UID;
             break;
     }
 
     try{
-        const Query = Stack.ContentType(CONTENT_TYPE_UID).Entry(ENTRY_UID);
-        const data=await Query.fetch()
+        const Query = Stack.ContentType(CONTENT_TYPE_UID).Query();
+        let data=await Query.toJSON().find()
         if(data){
             sendResponse(
                 {
                     res,
                     statusCode:200,
                     message:"Data Fetched Successfully",
-                    data:data.toJSON()
+                    data:data
                 }
             )
         }
@@ -50,15 +48,17 @@ const fetchResuableComponentdata=async(req,res)=>{
 
 const gethomepagedata=async(req,res)=>{
     try{
-        const Query = Stack.ContentType(HOMEPAGE_CONTENT_TYPE_UID).Entry(HOMEPAGE_ENTRY_UID);
-        const data=await Query.includeReference("banner","market_recognition","client_logos").fetch()
+        const Query = Stack.ContentType(HOMEPAGE_CONTENT_TYPE_UID).Query();
+        const data=await Query
+                            .includeReference("banner","market_recognition","client_logos")
+                            .toJSON().find()
         if(data){
             sendResponse(
                 {
                     res,
                     statusCode:200,
                     message:"Data Fetched Successfully",
-                    data:data.toJSON()
+                    data:data
                 }
             )
         }
